ecd2a3c48216598f1a539b6f82e44ed8da830ef1
Marcos Vinicius Almeida Guimaraes <mvaguimaraes@gmail.com>
2021-05-13 08:42:43 -0300

    adding POJOs. Closes #2.

---
+++ src/main/java/guru/springframework/sfgpetclinic/model/Person.java
@@ -0,0 +1,23 @@
+package guru.springframework.sfgpetclinic.model;
+
+public class Person {
+
+    private String firstName;
+    private String lasteName;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLasteName() {
+        return lasteName;
+    }
+
+    public void setLasteName(String lasteName) {
+        this.lasteName = lasteName;
+    }
+}
